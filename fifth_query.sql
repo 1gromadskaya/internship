60e17a96b95b2f90cf14a8e2e487be3f0d8c9ad4
unknown <fpm.gromadsk@bsu.by>
2026-01-26 18:45:58 +0300

    Add new fifth and seventh query

--- fifth_query.sql
+++ fifth_query.sql
@@ -1,8 +1,15 @@
-SELECT CONCAT(actor.first_name, ' ', actor.last_name) AS full_name, COUNT(film_actor.actor_id) AS films_count FROM actor
-JOIN film_actor ON film_actor.actor_id = actor.actor_id
-JOIN film_category ON film_category.film_id = film_actor.film_id
-JOIN category ON category.category_id = film_category.category_id
-WHERE category.name = 'Children'
-GROUP BY actor.actor_id, actor.first_name, actor.last_name
-ORDER BY films_count DESC
-LIMIT 3
+WITH actor_stats AS (
+    SELECT 
+        CONCAT(a.first_name, ' ', a.last_name) AS full_name, 
+        COUNT(fa.film_id) AS films_count,
+        DENSE_RANK() OVER (ORDER BY COUNT(fa.film_id) DESC) as rank
+    FROM actor a
+    JOIN film_actor fa ON a.actor_id = fa.actor_id
+    JOIN film_category fc ON fa.film_id = fc.film_id
+    JOIN category c ON fc.category_id = c.category_id
+    WHERE c.name = 'Children'
+    GROUP BY a.actor_id, a.first_name, a.last_name
+)
+SELECT full_name, films_count
+FROM actor_stats
+WHERE rank <= 3;
